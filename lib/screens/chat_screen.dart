344b576b2cc2a01c478ab57c7b3f8b88fa8c8d96
Siddharth Shenoy <56694590+siddharthshenoy@users.noreply.github.com>
2021-05-08 21:51:19 +0530

    firebase DB 8may21 9:51

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -12,11 +12,15 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+
+  final _firestore = FirebaseFirestore.instance;
+
+  String messageText;
+
   final messageTextController = TextEditingController();
   final _auth = FirebaseAuth.instance;
   User loggedInUser;
-
-  String messageText;
+  
 
   @override
   void initState() {
@@ -65,14 +69,17 @@ class _ChatScreenState extends State<ChatScreen> {
                   Expanded(
                     child: TextField(
                       onChanged: (value) {
-                        //Do something with the user input.
+                        messageText = value;
                       },
                       decoration: kMessageTextFieldDecoration,
                     ),
                   ),
                   TextButton(
                     onPressed: () {
-                      //Implement send functionality.
+                      _firestore.collection('messages').add({
+                        'text':messageText,
+                        'sender': loggedInUser.email,
+                      });
                     },
                     child: Text(
                       'Send',
